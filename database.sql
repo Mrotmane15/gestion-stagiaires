050546f97fac843f090347444e4b5ed891e5e94e
KamalOS <noreply@kamal.com>
2024-11-21 14:27:50 +0100

    update database

--- database.sql
+++ database.sql
@@ -1,21 +1,15 @@
-CREATE DATABASE gestion_stagiaires;
+CREATE DATABASE ISTA;
 
-USE gestion_stagiaires;
+USE ISTA;
 
 CREATE TABLE stagiaires (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    matricule INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     nom VARCHAR(50) NOT NULL,
     prenom VARCHAR(50) NOT NULL,
-    genre ENUM('Homme', 'Femme') NOT NULL,
-    cin VARCHAR(11) NOT NULL,
-    cne VARCHAR(15) NOT NULL,
-    email VARCHAR(320) NOT NULL,
-    date_naissance date NOT NULL,
-    lieu_naissance VARCHAR(255) NOT NULL,
-    phone VARCHAR(10) NOT NULL,
-    niveau VARCHAR(20) NOT NULL,
     filiere VARCHAR(20) NOT NULL,
-    groupe VARCHAR(20) NOT NULL
+    annee_etude date NOT NULL,
+    type_bac VARCHAR(255) NOT NULL,
+    annee_bac VARCHAR(255) NOT NULL
 );
 
 
